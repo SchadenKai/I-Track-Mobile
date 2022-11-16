bd828413aa1f7967d2aceb05c9c1e4a8ea493a5d
SchadenKai <kairustecsonpersonal@gmail.com>
2022-11-16 11:26:26 +0800

    initial layouts

---
+++ src/pages/Home.tsx
@@ -0,0 +1,22 @@
+import { IonContent, IonHeader, IonPage , IonToolbar } from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Home.css';
+
+const Tab3: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader translucent={true}>
+          <IonToolbar>
+            <div className="container">
+              I-Track
+            </div>
+          </IonToolbar>
+        </IonHeader>
+      <IonContent fullscreen>
+        <ExploreContainer name="Tab 3 page" />
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Tab3;
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,21 +1,18 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
-import './Tab3.css';
+import './Profile.css';
 
 const Tab3: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
+      <IonHeader translucent={true}>
           <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
+            <div className="container">
+              I-Track
+            </div>
           </IonToolbar>
         </IonHeader>
+      <IonContent fullscreen>
         <ExploreContainer name="Tab 3 page" />
       </IonContent>
     </IonPage>
